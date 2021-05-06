e0eb66c32e5f356d76d042d3df0566c3bb86c54d
cmdoret <cyril.mattheydoret@gmail.com>
2021-05-06 02:16:54 +0200

    add function to compute local pixel density in sparse matrices

--- pareidolia/detection.py
+++ pareidolia/detection.py
@@ -92,35 +92,67 @@ def get_sse_mat(mats: Iterable[sp.spmatrix]) -> sp.spmatrix:
     return sse
 
 
-def get_win_density(mat, winsize=3, sym_upper=False):
-    """Compute pixel density in local windows using convolution. The convolution
-    is performed in 'valid' mode: only positions where the kernel can be centered
-    are computed, edges are left as 0.
+def get_win_density(
+    mat: sp.csr_matrix, win_size: int = 3, sym_upper: bool = False
+) -> sp.csr_matrix:
+    """Compute local pixel density in sparse matrices using convolution.
+    The convolution is performed in 'full' mode: Computations are performed
+    all the way to the edges by trimming the kernel.
 
-    Examples
-    --------
-    >>> mat = np.array(
-            [[0, 0, 0, 0, 1],
-             [0, 1, 1, 1, 1],
-             [0, 0, 0, 0, 1],
-             [0, 0, 0, 0, 1],
-             [0, 0, 0, 0, 0]]
-    )
-    >>> get_win_density(mat, winsize=3)
-    array([[0., 0., 0., 0., 0.],
-           [0., 2., 3., 5., 0.],
-           [0., 2., 3., 5., 0.],
-           [0., 0., 0., 2., 0.],
-           [0., 0., 0., 0., 0.]])
+    Parameters
+    ----------
+    mat : scipy.sparse.csr_matrix
+        The input sparse matrix to convolve.
+    win_size : int
+        The size of the area in which to compute the proportion of nonzero
+        pixels. This will be the kernel size used in convolution.
+    sym_upper : bool
+        Whether the input matrix is symmetric upper, in which case only the
+        upper triangle is returned.
+
+
+    Returns
+    -------
+    scipy.sparse.csr_matrix:
+        The result of the convolution of the uniform kernel
+        (win_size x win_size) on the binarized input matrix.
+        Each value represents the proportion of nonzero pixels
+        in the neighbourhood.
     """
+    ms, ns = mat.shape
     # Generate a binary matrix (pixels are either empty or full)
     bin_mat = mat.copy()
+    if sym_upper:
+        bin_mat = sp.triu(bin_mat)
     bin_mat.data = bin_mat.data.astype(bool)
-
-    # Convolve the uniform kernel with this matrix to get the count of nonzero
+    # Adding a frame of zeros around the signal
+    tmp = sp.csr_matrix((win_size - 1, ns), dtype=bool)
+    bin_mat = sp.vstack([tmp, bin_mat, tmp], format=mat.format)
+    tmp = sp.csr_matrix((ms + 2 * (win_size - 1), win_size - 1), dtype=bool)
+    bin_mat = sp.hstack([tmp, bin_mat, tmp], format=mat.format)
+    # Convolve the uniform kernel with this matrix to get the proportion of nonzero
     # pixels in each neighbourhood
-    kernel = np.ones((winsize, winsize))
-    density = cud.xcorr2(bin_mat, kernel)
+    kernel = np.ones((win_size, win_size))
+    win_area = win_size ** 2
+    density = cud.xcorr2(bin_mat, kernel / win_area)
 
+    # Compute convolution of uniform kernel with a frame of ones to get number
+    # of missing pixels in each window.
+    frame = cup.frame_missing_mask(
+        sp.csr_matrix(mat.shape, dtype=bool), kernel.shape, sym_upper=sym_upper
+    )
+    frame = cud.xcorr2(frame, kernel).tocoo()
+    # From now on, frame.data contains the number of 'present' samples.
+    # (where there is at least one missing pixel)
+    frame.data = win_area - frame.data
+
+    # Adjust the proportion for values close to the border (lower denominator)
+    density[frame.row, frame.col] = (
+        density[frame.row, frame.col].A1 * win_area / frame.data
+    )
+    # Trim the frame out from the signal
+    rad = win_size // 2 + 1
+    density = density[rad:-rad, rad:-rad]
+    if sym_upper:
+        density = sp.triu(density)
     return density
-
