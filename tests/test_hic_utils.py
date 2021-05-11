fd49a426501ee51726d0b890e3afbae2f9f61639
cmdoret <cyril.mattheydoret@gmail.com>
2021-05-11 22:05:11 +0200

    add unit tests for border detection

--- tests/test_hic_utils.py
+++ tests/test_hic_utils.py
@@ -17,10 +17,13 @@ DATA = pathlib.Path("data_test")
 # Synthetic matrices and their known loop coordinates
 COOLS = [str(c) for c in DATA.glob("B_[1-6]*.cool")]
 LOOPS = np.loadtxt(DATA / "B_loops.txt")
+BORDERS = np.loadtxt(DATA / "B_borders.txt")
 # Matrices with a diagonal gradient
 COOLS_COMP = [str(c) for c in DATA.glob("smooth_[1-6]*.cool")]
 COOL_IN = ("cool", COOLS)
 REGION = "chr0:100000-120000"
+# Fixture for testing different patterns
+PATTERNS = ("kernel,coords", [("loops", LOOPS), ("borders", BORDERS)])
 
 
 def test_get_min_contacts():
@@ -62,13 +65,14 @@ def test_coords_to_bins():
     np.testing.assert_equal(obs_idx, exp_idx)
 
 
-def test_change_detection():
+@pytest.mark.parametrize(*PATTERNS)
+def test_change_detection(kernel, coords):
     """Test if change detection pipeline finds some relevant positions"""
     # Run loop change detection between matrices with and without loops
     cools = COOLS + COOLS_COMP
     conds = ["B"] * len(COOLS) + ["S"] * len(COOLS_COMP)
     obs_pos = pah.change_detection_pipeline(
-        cools, conds, subsample=False, mode="median"
+        cools, conds, subsample=False, kernel=kernel
     )
     # Build a set of fuzzy (+/3 pixels around) positions found
     valid_pos = set()
@@ -77,11 +81,11 @@ def test_change_detection():
             valid_pos.add((pos[0] + shift[0], pos[1] + shift[1]))
     # Count the number of real loop positions that were found
     found = 0
-    for target in LOOPS:
+    for target in coords:
         if tuple(target.astype(int)) in valid_pos:
             found += 1
-    print(f"Found {found} out of {LOOPS.shape[0]} loops.")
-    assert found / LOOPS.shape[0] >= 0.5
+    print(f"Found {found} out of {coords.shape[0]} loops.")
+    assert found / coords.shape[0] >= 0.5
 
 
 def test_change_quantification():
@@ -90,7 +94,10 @@ def test_change_quantification():
     cools = COOLS + COOLS_COMP
     conds = ["B"] * len(COOLS) + ["S"] * len(COOLS_COMP)
     obs_pos = pah.change_detection_pipeline(
-        cools, conds, bed2d_file=str(DATA / "B_loops.bed2d"), subsample=False,
+        cools,
+        conds,
+        bed2d_file=str(DATA / "B_loops.bed2d"),
+        subsample=False,
     )
     diff = obs_pos.diff_score
     # Check if change was detected in the correct direction (disappearing)
@@ -98,18 +105,20 @@ def test_change_quantification():
     assert len(diff[diff < 0]) >= len(diff) * 0.3
 
 
-def test_change_no_threshold():
+@pytest.mark.parametrize("kernel", ("loops", "borders"))
+def test_change_no_threshold(kernel):
     """Test if change detection pipeline without threshold reports all input positions"""
     # Run loop change detection between matrices with and without loops
     cools = COOLS + COOLS_COMP
     conds = ["B"] * len(COOLS) + ["S"] * len(COOLS_COMP)
-    bed2d = pd.read_csv(str(DATA / "B_loops.bed2d"), sep="\t")
+    bed2d = pd.read_csv(str(DATA / f"B_{kernel}.bed2d"), sep="\t")
     obs_pos = pah.change_detection_pipeline(
         cools,
         conds,
-        bed2d_file=str(DATA / "B_loops.bed2d"),
+        bed2d_file=str(DATA / f"B_{kernel}.bed2d"),
         subsample=False,
         pearson_thresh=None,
+        kernel=kernel,
     )
     diff = obs_pos.diff_score
     # Check if diff scores are returned for all positions
