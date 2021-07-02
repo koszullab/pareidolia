fb872d0da4b1ad355a14b38da4c4363ffe7774ca
cmdoret <cyril.mattheydoret@gmail.com>
2021-07-02 18:37:45 +0200

    bump to v0.6.1

--- pareidolia/__init__.py
+++ pareidolia/__init__.py
@@ -1,5 +1,5 @@
 """pareidolia - Multi-sample change detection in Hi-C patterns"""
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __author__ = "Cyril Matthey-Doret <cyril.matthey-doret@pasteur.fr>"
 __all__ = []
